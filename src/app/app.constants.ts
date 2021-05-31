14bd3f776e4c3930dafdd778ef5f006165e4e14d
jelenapetrovacki <jpetrovacki3@gmail.com>
2021-05-31 16:37:48 +0200

    Enpoints updated; _redirects file created

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,10 +1,16 @@
+/*
 export const ARTIKL_URL = 'http://localhost:8083/artikl';
 export const DOBAVLJAC_URL = 'http://localhost:8083/dobavljac';
 export const PORUDZBINA_URL = 'http://localhost:8083/porudzbina';
 
 export const STAVKA_PORUDZBINE_URL = 'http://localhost:8083/stavkaPorudzbine';
 export const STAVKE_ZA_PORUDZBINU_URL = 'http://localhost:8083/stavkeZaPorudzbinaID';
+*/
 
-
+export const ARTIKL_URL = 'https://rva-backend-grupa1.herokuapp.com/artikl';
+export const DOBAVLJAC_URL = 'https://rva-backend-grupa1.herokuapp.com/dobavljac';
+export const PORUDZBINA_URL = 'https://rva-backend-grupa1.herokuapp.com/porudzbina';
+export const STAVKA_PORUDZBINE_URL = 'https://rva-backend-grupa1.herokuapp.com/stavkaPorudzbine';
+export const STAVKE_ZA_PORUDZBINU_URL = 'https://rva-backend-grupa1.herokuapp.com/stavkeZaPorudzbinaID';
 
 
